86c59eac7b0bc500dc7943eec07af2ce0f35ff0e
19_web_ksy <19_web_ksy@DESKTOP-GDNLM1E>
2022-04-20 21:45:08 +0900

    Add bList and getOneBoard in BoardDao

--- 99_practice/WebContent/pract3_board/bList.jsp
+++ 99_practice/WebContent/pract3_board/bList.jsp
@@ -1,3 +1,6 @@
+<%@page import="boardEx.BoardDao"%>
+<%@page import="boardEx.BoardDto"%>
+<%@page import="java.util.ArrayList"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
@@ -6,6 +9,12 @@
 <title>b list</title>
 </head>
 <body>
+
+	<h1>게시글 보기</h1>
+
+	<%
+		ArrayList<BoardDto> boardList = BoardDao.getInstance().getBoardList();
+	%>
 	<table border="1">
 		<tr>
 			<td>번호</td>
@@ -14,13 +23,19 @@
 			<td>작성일</td>
 			<td>조회수</td>
 		</tr>
-		<tr>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td></td>
-		</tr>
+	<%
+			for (BoardDto boardDto : boardList) {
+	%>
+				<tr>
+					<td><%=boardDto.getNum() %></td>
+					<td><a href="bInfo.jsp?num=<%=boardDto.getNum() %>"><%=boardDto.getSubject() %></a></td>
+					<td><%=boardDto.getWriter() %></td>
+					<td><%=boardDto.getRegDate() %></td>
+					<td><%=boardDto.getReadCount() %></td>
+				</tr>
+	<%
+			}
+	%>
 		<tr>
 			<td colspan="5"><input type="button" value="글쓰기" onclick="location.href = 'bWrite.jsp'"></td>
 		</tr>
--- 99_practice/src/boardEx/BoardDao.java
+++ 99_practice/src/boardEx/BoardDao.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class BoardDao {
 	
@@ -60,6 +61,88 @@ public class BoardDao {
 			if (conn != null) {try {conn.close();}  catch (SQLException e) {}}
 		}
 
+	}
 	
+	public ArrayList<BoardDto> getBoardList() {
+		ArrayList<BoardDto> boardList = new ArrayList<BoardDto>();
+		
+		try {
+			conn = getConnection();
+			
+			pstmt = conn.prepareStatement("SELECT * FROM BOARD");
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				BoardDto boardDto = new BoardDto();
+				
+				boardDto.setNum(rs.getInt("NUM"));
+				boardDto.setWriter(rs.getString("WRITER"));
+				boardDto.setEmail(rs.getString("EMAIL"));
+				boardDto.setSubject(rs.getString("SUBJECT"));
+				boardDto.setPassword(rs.getString("PASSWORD"));
+				boardDto.setRegDate(rs.getDate("REG_DATE"));
+				boardDto.setReadCount(rs.getInt("READ_COUNT"));
+				boardDto.setContent("CONTENT");
+				
+				boardList.add(boardDto);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return boardList;
+	}
+	
+	public BoardDto getOneBoard(int num) {
+		BoardDto boardDto = new BoardDto();
+		
+		try {
+			conn = getConnection();
+			
+			pstmt = conn.prepareStatement("SELECT * FROM BOARD WHERE NUM = ?");
+			pstmt.setInt(1, num);
+			rs = pstmt.executeQuery();
+			
+			boardDto.setNum(rs.getInt("NUM"));
+			boardDto.setWriter(rs.getString("WRITER"));
+			boardDto.setEmail(rs.getString("EMAIL"));
+			boardDto.setSubject(rs.getString("SUBJECT"));
+			boardDto.setPassword(rs.getString("PASSWORD"));
+			boardDto.setRegDate(rs.getDate("REG_DATE"));
+			boardDto.setReadCount(rs.getInt("READ_COUNT"));
+			boardDto.setContent(rs.getString("CONTENT"));
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return boardDto;
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
